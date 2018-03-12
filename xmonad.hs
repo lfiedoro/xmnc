8dbeef53e26796b48ff869e87e6302d1405cf4b8
Lukasz Fiedorowicz <lukasz.fiedorowicz@intel.com>
2018-03-12 14:13:05 +0100

    Big overhaul. Finaly works as I want it to work

--- xmonad.hs
+++ xmonad.hs
@@ -1,25 +1,17 @@
+import System.IO
 import XMonad
+import XMonad.Actions.Warp
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
 
-main = do
-    xmproc <- spawnPipe "xmobar"
-    xmonad $ defaultConfig
-        { borderWidth = 3
-        , terminal = "st"
-        , normalBorderColor  = "#cccccc"
-        , focusedBorderColor = "#cd8b00"
-        , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
-        } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xlock")
-        , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack:pixelsize=13:antialias=true:hinting=true' -nb '#002b36' -nf '#839496' -sb '#073642' -sf '#b58900'")
-        ]
+main = xmonad =<< xmobar myConfig
+
+myConfig = def { modMask = mod4Mask
+               , normalBorderColor  = "#cccccc"
+               , focusedBorderColor = "#cd8b00"
+               , terminal = "st"
+               } `additionalKeys`
+               [ ((mod4Mask .|. shiftMask, xK_z), spawn "xlock")
+               , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack:pixelsize=13:antialias=true:hinting=true' -nb '#002b36' -nf '#839496' -sb '#073642' -sf '#b58900'")
+               ]
