1af839ea8225f33b252b89f161862b42b5813ac9
Lukasz Fiedorowicz <lfiedoro@lfiedoro-desk1.igk.intel.com>
2018-03-12 12:17:20 +0100

    Further improvements

--- xmonad.hs
+++ xmonad.hs
@@ -1,15 +1,24 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
+    xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { borderWidth = 3
         , terminal = "st"
         , normalBorderColor  = "#cccccc"
         , focusedBorderColor = "#cd8b00"
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xlock")
         , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack:pixelsize=13:antialias=true:hinting=true' -nb '#002b36' -nf '#839496' -sb '#073642' -sf '#b58900'")
