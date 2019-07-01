f6db9009243baf6821da588c284891999cce2c5e
Lukasz Fiedorowicz <lukasz.fiedorowicz@intel.com>
2019-07-01 10:32:21 +0200

    Simplify

--- xmonad.hs
+++ xmonad.hs
@@ -8,10 +8,8 @@ import XMonad.Util.EZConfig(additionalKeys)
 main = xmonad =<< xmobar myConfig
 
 myConfig = def { modMask = mod4Mask
-               , normalBorderColor  = "#cccccc"
-               , focusedBorderColor = "#cd8b00"
                , terminal = "st"
                } `additionalKeys`
                [ ((mod4Mask .|. shiftMask, xK_z), spawn "xlock")
-               , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack:pixelsize=13:antialias=true:hinting=true' -nb '#002b36' -nf '#839496' -sb '#073642' -sf '#b58900'")
+               , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Hack:pixelsize=13:antialias=true:hinting=true'")
                ]
